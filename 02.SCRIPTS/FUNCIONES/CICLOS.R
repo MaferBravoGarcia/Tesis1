dfd7b9a19eeefc3d11a1f7e7b2e5f21ff5c4bca9
MaferBravoGarcia <mafer170497@gmail>
2021-08-06 15:30:02 -0500

    Variables y ciclo para modelo Ricker. Genera graficas

--- 02.SCRIPTS/FUNCIONES/CICLOS.R
+++ 02.SCRIPTS/FUNCIONES/CICLOS.R
@@ -10,18 +10,20 @@ library(igraph)
 
 #Matriz 
 N = 10 
-pep = 34
-A = generateA(N,"klemm", pep = pep, c = 0.05)
+pep = 10
+conec = 0.05
+A = generateA(N,"klemm", pep = pep, c = conec)
 
 ##Ricker
 tend = 15
-sigm = c(0.01, 0.05, 0.1)
+sigm = c(0.01,0.05,0.1)
+cap = 0.01
+
 for (sig in sigm) {
-  pdf(paste("Ricker", sig, ".pdf"))
-  rickerplot=tsplot(ricker(N,A=A,K=rep(0.01,N), tend=tend, sigma = sig), main = paste("Ricker", sig)) 
-  setwd("~/Microtesis/Tesis1/03.OUTPUT/PLOTS/")
-  save(rickerplot, file = paste("Ricker", sig, ".RData"))
+  pdf(paste("Ricker", "N =", N, "Tiempo = ", tend, "pep =", pep, "c =", conec, "sigma =", sig, "K =", cap, ".pdf"))
+  rickerplot=tsplot(ricker(N,A=A,K=rep(cap,N), tend=tend, sigma = sig), main = paste("Ricker", "%ep =", pep, "c =", conec, "σ =", sig, "K =", cap), legend = TRUE ) 
   dev.off() 
-  
+  setwd("~/Microtesis/Tesis1/03.OUTPUT/PLOTS/RICKER/")
+  save(rickerplot, file = paste("Ricker", "N =", N, "Tiempo = ", tend, "pep =", pep, "c =", conec, "sigma =", sig, "K =", cap, ".RData"))
 }
 
