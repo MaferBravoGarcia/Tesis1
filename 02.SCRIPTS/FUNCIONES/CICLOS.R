db493bc9fcacdf2d4bd8c9aa4a3bcb6a4c487699
MaferBravoGarcia <mafer170497@gmail>
2021-08-10 15:35:12 -0500

    Ciclo y generacion de graficas y modelo Hubbell. Variables separadas.

--- 02.SCRIPTS/FUNCIONES/CICLOS.R
+++ 02.SCRIPTS/FUNCIONES/CICLOS.R
@@ -18,7 +18,7 @@ A = generateA(N,"klemm", pep = pep, c = conec)
 tend = 15
 
 ##Ricker
-sigm = c(0.01,0.05,0.1)
+sigm = c(0, 0.01,0.05,0.1)
 cap = 0.01
 
 for (sig in sigm) {
@@ -35,7 +35,7 @@ grmin = 0
 grmax = 0.5
 
 pdf(paste("gLV", "N =", N, "Tiempo = ", tend, "pep =", pep, "c =", conec, "tasa crecimiento", grmin, "-", grmax, ".pdf"))
-glvplot=tsplot(glv (N, A=A, tend=tend, b = runif(N, min = grmin, max = grmax)), main = paste("gLV", "%ep =", pep, "c =", conec, "tasa crecimiento", grmin, "-", grmax), legend = TRUE )
+glvplot=tsplot(glv (N, A=A, tstep = 1, tend=tend, b = runif(N, min = grmin, max = grmax)), main = paste("gLV", "%ep =", pep, "c =", conec, "tasa crecimiento", grmin, "-", grmax), legend = TRUE )
 dev.off() 
 setwd("~/Microtesis/Tesis1/03.OUTPUT/PLOTS/gLV/")
 save(glvplot, file = paste("gLV", "N =", N, "Tiempo = ", tend, "pep =", pep, "c =", conec, "tasa crecimiento", grmin, "-", grmax,".RData"))
@@ -50,3 +50,29 @@ soiplot=tsplot(soi(N, A=A, I=ind, tend=tend, e.vector = runif(N, min = emin, max
 dev.off() 
 setwd("~/Microtesis/Tesis1/03.OUTPUT/PLOTS/SOI/")
 save(soiplot, file = paste("SOI", "N =", N, "Tiempo = ", tend, "pep =", pep, "c =", conec, "Individuos", ind, ".RData"))
+
+##HUBBELL
+ind = 200
+M = 10
+mig = c(0.1, 0.3, 0.5, 0.9, 0.98)
+skip = 5 
+death = c(10,100,1000)
+
+for (m in mig) {
+  for (muerte in death) {
+    pdf(paste("Hubbell", "N =", N, "Tiempo = ", tend, "pep =", pep, "c =", conec,  "Individuos", ind, "metacom", M, "Tasa inmigra", m, "skip", skip, "#muertes", muerte,  ".pdf"))
+    metapop <- generateAbundances(N = M, mode = 5, probabs = TRUE)
+    hubplot = tsplot(simHubbell(N = N, M = M, I = ind, d = muerte, m=m,  m.vector = metapop, tskip=skip, tend=tend), main = paste("Hubbell", "%ep =", pep, "c =", conec, "Individuos", ind, "metacom", M,"Tasa inmigra", m, "skip", skip, "#muertes", muerte), legend = TRUE )
+    dev.off() 
+    setwd("~/Microtesis/Tesis1/03.OUTPUT/PLOTS/HUBBELL/")
+    save(hubplot, file = paste("Hubbell", "N =", N, "Tiempo = ", tend, "pep =", pep, "c =", conec,  "Individuos", ind, "metacom", M, "Tasa inmigra", m, "skip", skip, "#muertes", muerte, ".RData"))
+     }
+}
+
+
+
+
+
+
+
+
